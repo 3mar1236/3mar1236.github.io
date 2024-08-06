1372ad09b9e5fe2c97048ac320a3ecaa5b8b2acb
4mar <omarnoury9@gmail.com>
2024-08-06 10:17:16 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -4,8 +4,16 @@ document.addEventListener('scroll', () => {
     // window.scrollY > 1? navHi.classList.add('scrolled') : navHi.classList.remove('scrolled');
     if (window.scrollY > 40){
         navHi.classList.add('scrolled')
-    } else if (window.scrollY <= 0){
+    } else if (window.scrollY == 0){
         // setTimeout(() => navHi.classList.remove('scrolled'), 1000);
         navHi.classList.remove('scrolled');
     }
+})
+
+const darkBtn = document.querySelector('.darkmode-button');
+const html = document.querySelector('html');
+
+darkBtn.addEventListener('click', () => {
+    html.classList.toggle('dark-mode');
+    darkBtn.classList.toggle('dark-mode');
 })
