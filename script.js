7cd9b984e610162cc925e590bbe83c098965c8a9
4mar <omarnoury9@gmail.com>
2024-08-05 22:27:35 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener('scroll', () => {
     // window.scrollY > 1? navHi.classList.add('scrolled') : navHi.classList.remove('scrolled');
     if (window.scrollY > 40){
         navHi.classList.add('scrolled')
-    } else if (window.scrollY == 0){
+    } else if (window.scrollY <= 0){
         // setTimeout(() => navHi.classList.remove('scrolled'), 1000);
         navHi.classList.remove('scrolled');
     }
